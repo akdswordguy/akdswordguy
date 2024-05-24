e1682f72bfcf274f770e020c7c34f211b9de9b5e
D Aditya Kiran <150567695+akdswordguy@users.noreply.github.com>
2024-05-24 22:15:28 +0530

    Add files via upload
    This files contains the mongodb and node js  code for creating a login signup page and code for creating team members list with due date and event name (task management system team mangaer). Hope it will be useful for you all.

---
+++ mongodb.js
@@ -0,0 +1,29 @@
+
+//mongoose password is 3k8LRm74PKNcShRl
+
+
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb+srv://akdelite:3k8LRm74PKNcShRl@cluster0.t0tfwj8.mongodb.net/loginn'; 
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose.connection; // Export the connection objectx
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+
+
+
+module.exports=LogInCollection
